02df85d5c76d66ad806bc060a170099557643954
fulbrighte23 <fulbrighte23@greenhill.org>
2020-06-26 14:51:22 -0500

    June 23, 2020

--- src/com/company/Chlallenge2.java
+++ src/com/company/Chlallenge2.java
@@ -2,16 +2,7 @@ package com.company;
 
 public class Chlallenge2 {
     public static void main(String[] args) {
-        line();
 
-        line();
-
-    }
-
-    public static void line() {
-        System.out.print("+");
-        for (int dash = 1; dash <5*2; dash++) ;
-        System.out.print("-");
-        System.out.print("+");
+        System.out.println("+---------+");
     }
 }
